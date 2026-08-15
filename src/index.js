650c79f5b876264ad46c87ddf48fd391849eb836
Peter <224707854+Peter-Paul-Dev@users.noreply.github.com>
2026-08-15 16:55:57 +0800

    feat: Added feature that lets a user create a new todo from the UI.
    It is done using the "Create New Task" button on the sidebar. This button opens up a dialog box with a form inside it and that's where the user will input the necessary fields, like the title, due date, and description.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import "./style.css";
 import { allTodos, createTodo, findMatch, createNewProject, allProjects } from "./todo-logic.js";
 import { displaySection, displayTodo, displayTodoInProjects, createProjectList } from "./manipulate-dom.js";
 
-const myTask = createTodo("Finish Project", "Finish the project I'm working on", "Whenever", "Not urgent", "None");
+const myTask = createTodo("Finish Project", "Finish the project I'm working on", "Whenever", "Not Urgent", "None");
 
 const newArr = createNewProject("My New Project List");
 
-const newTask = createTodo("Test stuff", "Finish testing stuff", "When I feel like it", "Not urgent", "None");
+const newTask = createTodo("Test stuff", "Finish testing stuff", "When I feel like it", "Not Urgent", "None");
 const newerTask = createTodo("Buy food", "Buy food for tomorrow", "Tonight", "Urgent", "Remeber to buy mayo");
 
 newArr.addToProject("Test stuff");
@@ -25,3 +25,21 @@ displayTodoInProjects("All Tasks", allProjects);
 console.log(allProjects);
 console.log(allTodos);
 console.log(newArr);
+
+
+document.querySelector("#add-new-task").addEventListener("submit", function(e) {
+    e.preventDefault();
+    const formData = new FormData(this);
+
+    const userInputs = {};
+
+    for (const key of formData.keys()) {
+        if (formData.get(key).toString().length > 0) {
+            userInputs[key] = formData.get(key).toString();
+        }
+    }
+
+    console.log(userInputs);
+    const addedTask = createTodo(userInputs.title, userInputs.description, userInputs.dueDate, userInputs.priority, userInputs.notes);
+    displayTodoInProjects("All Tasks", allProjects);
+});
